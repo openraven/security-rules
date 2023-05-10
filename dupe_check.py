65090ade5f5fa75f3247b522bcb234a8b57e144c
Jason Nichols <jason@kickroot.com>
2023-05-10 19:02:11 -0400

    Handle files that don't start with the id on the first line

--- dupe_check.py
+++ dupe_check.py
@@ -1,6 +1,16 @@
 
 import os
 
+def extract_id(file):
+  with open(sub_path + file, 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+      line = line.strip().split(":")
+      if len(line) == 2 and line[0].strip() == "id":
+        return line[1]
+  return None
+
+
 print(f"Initializing in {os.getcwd()}")
 sub_path = "resources/rules/"
 id_to_files = {}
@@ -8,13 +18,14 @@ id_to_files = {}
 for file in os.listdir(sub_path):
   if file.endswith(".yaml"):
     print(f"Processing {file}")
-    with open(sub_path + file, 'r') as f:
-      line = f.readline().strip()
-      uuid = line.split(":")[1].strip()
+    uuid = extract_id(file)
+    if uuid is None:
+      print(f"Couldn't process {file}, ignoring.")
+    else :  
       if uuid in id_to_files.keys():
         msg = f"Duplicate key found for id={uuid}, file1={id_to_files[uuid]}, file2={file}"
         raise Exception(msg)
       id_to_files[uuid] = file
-print(f"Successfully validated {id_to_files.size()} files for duplicate IDs")
+print(f"Successfully validated {len(id_to_files)} files for duplicate IDs")
 
 
